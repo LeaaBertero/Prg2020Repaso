408c365d7ad4ff1d61e64397b140b06956eedc05
Leandro Bertero <leandro.bertero@hotmail.com>
2024-02-01 04:08:20 -0300

    comentarios en el codigo

--- operadores/operadores/operadores.cs
+++ operadores/operadores/operadores.cs
@@ -2,30 +2,34 @@ namespace operadores
 {
     public partial class Form1 : Form
     {
+        //definición de variables, en este caso enteras, por cuestión de práctica..
         int numero1;
         int numero2;
         int resultado;
 
-        public Form1()
+        //constructor
+        public Form1() //nombre del formulario(olvidé cambiarlo)
         {
             InitializeComponent();
         }
 
         private void btsumar_Click(object sender, EventArgs e)
         {
-
+            //control de errores
             try
             {
+                //variables definidas anteriormente, convertidas a entero, para poder realizar la sumatoria 
+                //con los respectivos texbox incluidos, donde el usuario va a ingresar los datos correspondientes
                 numero1 = Convert.ToInt32(txnum1.Text);
                 numero2 = Convert.ToInt32(txnum2.Text);
 
-
+                //suma de variables definidas al principio
                 resultado = numero1 + numero2;
 
+                //se convierte la variable resultado a string, donde se va a visulizar el resultado
                 lbresultado.Text = resultado.ToString();
 
-                //lbresultado.ToString(); 
-
+                //se muestra visible el resultado a realizar click en el boton sumar
                 lbresultado.Visible = true;
             }
             catch (Exception)
