13d6412d331580123ff5a30e8f1ff8e6999913a7
Leandro Bertero <leandro.bertero@hotmail.com>
2024-02-20 17:43:24 -0300

    prueba desplazamiento de botones

---
+++ proyectoEventosClick/proyectoEventosClick/EventosClick.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace proyectoEventosClick
+{
+    partial class EventosClick
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btIniciar = new Button();
+            btbajar = new Button();
+            btsalir = new Button();
+            SuspendLayout();
+            // 
+            // btIniciar
+            // 
+            btIniciar.Location = new Point(12, 71);
+            btIniciar.Name = "btIniciar";
+            btIniciar.Size = new Size(207, 46);
+            btIniciar.TabIndex = 0;
+            btIniciar.Text = "haca la derecha --->";
+            btIniciar.UseVisualStyleBackColor = true;
+            btIniciar.MouseMove += btIniciar_MouseMove;
+            // 
+            // btbajar
+            // 
+            btbajar.Location = new Point(256, 71);
+            btbajar.Name = "btbajar";
+            btbajar.Size = new Size(207, 46);
+            btbajar.TabIndex = 1;
+            btbajar.Text = "Hacia abajo";
+            btbajar.UseVisualStyleBackColor = true;
+            btbajar.MouseMove += button1_MouseMove;
+            // 
+            // btsalir
+            // 
+            btsalir.Location = new Point(581, 392);
+            btsalir.Name = "btsalir";
+            btsalir.Size = new Size(207, 46);
+            btsalir.TabIndex = 2;
+            btsalir.Text = "salir";
+            btsalir.UseVisualStyleBackColor = true;
+            btsalir.Click += btsalir_Click;
+            // 
+            // EventosClick
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btsalir);
+            Controls.Add(btbajar);
+            Controls.Add(btIniciar);
+            Name = "EventosClick";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "EventosClick";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btIniciar;
+        private Button btbajar;
+        private Button btsalir;
+    }
+}
---
+++ proyectoEventosClick/proyectoEventosClick/EventosClick.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyectoEventosClick
+{
+    public partial class EventosClick : Form
+    {
+        public EventosClick()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btIniciar_MouseMove(object sender, MouseEventArgs e)
+        {
+            btIniciar.Left = btIniciar.Left + 5;
+            //this.Refresh();
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            btbajar.Top = btbajar.Top + 5;
+        }
+
+        private void btsalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
