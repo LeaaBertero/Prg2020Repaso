0398ae2b8a543ff807bb0141e798bf0c524f48cd
Leandro Bertero <leandro.bertero@hotmail.com>
2024-02-21 11:35:31 -0300

    Prueba reloj (Timer)

---
+++ Timer_prueba/Timer_prueba/Timer.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Timer_prueba
+{
+    partial class Timer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            timer1 = new System.Windows.Forms.Timer(components);
+            lbTimer = new Label();
+            btcerrar = new Button();
+            SuspendLayout();
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // lbTimer
+            // 
+            lbTimer.AutoSize = true;
+            lbTimer.BackColor = Color.DarkOrange;
+            lbTimer.Cursor = Cursors.No;
+            lbTimer.Font = new Font("Kristen ITC", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbTimer.Location = new Point(190, 28);
+            lbTimer.Name = "lbTimer";
+            lbTimer.Size = new Size(119, 45);
+            lbTimer.TabIndex = 0;
+            lbTimer.Text = "label1";
+            lbTimer.Visible = false;
+            lbTimer.Click += lbTimer_Click;
+            // 
+            // btcerrar
+            // 
+            btcerrar.Cursor = Cursors.Hand;
+            btcerrar.ForeColor = Color.Black;
+            btcerrar.Location = new Point(634, 177);
+            btcerrar.Name = "btcerrar";
+            btcerrar.Size = new Size(94, 29);
+            btcerrar.TabIndex = 1;
+            btcerrar.Text = "Salir";
+            btcerrar.UseVisualStyleBackColor = true;
+            btcerrar.Click += btcerrar_Click;
+            // 
+            // Timer
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(740, 218);
+            Controls.Add(btcerrar);
+            Controls.Add(lbTimer);
+            ForeColor = Color.White;
+            Name = "Timer";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Timer";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private Label lbTimer;
+        private Button btcerrar;
+    }
+}
---
+++ Timer_prueba/Timer_prueba/Timer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer_prueba
+{
+    public partial class Timer : Form
+    {
+        //int contador;
+        public Timer()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //contador = contador + 1;
+            lbTimer.Text = "La hora es:" + " " + DateTime.Now.ToString("hh:mm:ss");
+            //lbTimer.Text = DateTime.Now.ToString();
+            lbTimer.Visible = true;
+        }
+
+        private void lbTimer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btcerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
