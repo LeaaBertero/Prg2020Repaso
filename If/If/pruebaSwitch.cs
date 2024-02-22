67f50d1e6619ca2b4102cbb2a5df2d3f1cbb3bc9
Leandro Bertero <leandro.bertero@hotmail.com>
2024-02-22 12:18:22 -0300

    se trasladaron las variables de todas las operaciones de locales a globales

--- If/If/pruebaSwitch.cs
+++ If/If/pruebaSwitch.cs
@@ -12,6 +12,30 @@ namespace If
 {
     public partial class pruebaSwitch : Form
     {
+        //variables para la suma
+        int numSuma1;
+        int numSuma2;
+
+        int resultadoSuma;
+
+        //variables para la resta
+        int numeroRes1;
+        int numeroRes2;
+
+        int resultadoRes;
+
+        //variables para la multiplicación
+        int numeroMult1;
+        int numeroMult2;
+
+        int resultadoMult;
+
+        //Variables para división
+        int numeroDiv1;
+        int numeroDiv2;
+
+        int resultadoDiv;
+
         public pruebaSwitch()
         {
             InitializeComponent();
@@ -28,59 +52,47 @@ namespace If
             switch (txoperador.Text)
             {
                 case "+":
-                    int numSuma1;
-                    int numSuma2;
-
-                    int resultado;
-
+                   
                     numSuma1 = Convert.ToInt32(txnum1.Text);
                     numSuma2 = Convert.ToInt32(txnum2.Text);
 
-                    resultado = numSuma1 + numSuma2;
+                    resultadoSuma = numSuma1 + numSuma2;
 
-                    lbresSwitch.Text = resultado.ToString();
+                    lbresSwitch.Text = resultadoSuma.ToString();
+
                 break;
                 
                 case "-":
-                    int numeroRes1;
-                    int numeroRes2;
-
-                    int resultadoRes;
-
+                   
                     numeroRes1 = Convert.ToInt32(txnum1.Text);
                     numeroRes2 = Convert.ToInt32(txnum2.Text);
 
                     resultadoRes = numeroRes1 - numeroRes2;
 
                     lbresSwitch.Text = resultadoRes.ToString();
+
                 break;
 
                 case "*":
-                    int numeroMult1;
-                    int numeroMult2;
-
-                    int resultadoMult;
-
+                   
                     numeroMult1 = Convert.ToInt32(txnum1.Text);
                     numeroMult2 = Convert.ToInt32(txnum2.Text);
 
                     resultadoMult = numeroMult1 * numeroMult2;
 
                     lbresSwitch.Text = resultadoMult.ToString();
+
                 break;
 
                 case "/":
-                    int numeroDiv1;
-                    int numeroDiv2;
-
-                    int resultadoDiv;
-
+                   
                     numeroDiv1 = Convert.ToInt32(txnum1.Text);
                     numeroDiv2 = Convert.ToInt32(txnum2.Text);
 
                     resultadoDiv = numeroDiv1 / numeroDiv2;
 
                     lbresSwitch.Text = resultadoDiv.ToString();
+
                 break;
             }
 
