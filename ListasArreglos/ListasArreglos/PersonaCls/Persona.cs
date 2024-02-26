df1818528085b34e2847613bdd6c8702942e83c2
Leandro Bertero <leandro.bertero@hotmail.com>
2024-02-26 00:07:02 -0300

    modificación , arreglos en el codigo

--- ListasArreglos/ListasArreglos/PersonaCls/Persona.cs
+++ ListasArreglos/ListasArreglos/PersonaCls/Persona.cs
@@ -8,7 +8,7 @@ namespace ListasArreglos.PersonaCls
 {
     public class Persona
     { 
-        public string Nombre { get; set; }
+        public string? Nombre { get; set; }
         public int añoNacimiento { get; set; }
 
         public int Documento { get; set; }
