cc6e44f71f84fb1aa3a4c0a9cfbce415d2a0efd9
Leandro Bertero <leandro.bertero@hotmail.com>
2024-02-29 02:21:13 -0300

    correcion en la propiedad nmbre de la clase persona

--- RepasoPruebaLista/RepasoPruebaLista/Persona.cs
+++ RepasoPruebaLista/RepasoPruebaLista/Persona.cs
@@ -8,7 +8,7 @@ namespace RepasoPruebaLista
 {
     public class Persona
     {
-        public string Nombre { get; set; } 
+        public string? Nombre { get; set; } 
         //public string Apellido { get; set;}
 
     }
