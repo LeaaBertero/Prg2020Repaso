09e20106f2b9a357adc5b35e5f392d5e713da234
Leandro Bertero <leandro.bertero@hotmail.com>
2024-03-01 17:05:56 -0300

    prueba

---
+++ borrarRepaso/borrarRepaso/pruebaBorrar.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace borrarRepaso
+{
+    partial class pruebaBorrar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btcargar = new Button();
+            txcargar = new TextBox();
+            lbresultado = new Label();
+            label2 = new Label();
+            btdimensionar = new Button();
+            txDimensionar = new TextBox();
+            btsalir = new Button();
+            btmostrar = new Button();
+            SuspendLayout();
+            // 
+            // btcargar
+            // 
+            btcargar.Location = new Point(91, 185);
+            btcargar.Name = "btcargar";
+            btcargar.Size = new Size(100, 29);
+            btcargar.TabIndex = 0;
+            btcargar.Text = "Cargar";
+            btcargar.UseVisualStyleBackColor = true;
+            btcargar.Click += btcargar_Click;
+            // 
+            // txcargar
+            // 
+            txcargar.Location = new Point(264, 186);
+            txcargar.Name = "txcargar";
+            txcargar.Size = new Size(125, 27);
+            txcargar.TabIndex = 1;
+            // 
+            // lbresultado
+            // 
+            lbresultado.AutoSize = true;
+            lbresultado.Location = new Point(526, 194);
+            lbresultado.Name = "lbresultado";
+            lbresultado.Size = new Size(50, 20);
+            lbresultado.TabIndex = 2;
+            lbresultado.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Snap ITC", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(210, 29);
+            label2.Name = "label2";
+            label2.Size = new Size(341, 39);
+            label2.TabIndex = 3;
+            label2.Text = "Matrices - prueba";
+            // 
+            // btdimensionar
+            // 
+            btdimensionar.Location = new Point(91, 150);
+            btdimensionar.Name = "btdimensionar";
+            btdimensionar.Size = new Size(100, 29);
+            btdimensionar.TabIndex = 4;
+            btdimensionar.Text = "Dimensionar";
+            btdimensionar.UseVisualStyleBackColor = true;
+            btdimensionar.Click += btdimensionar_Click;
+            // 
+            // txDimensionar
+            // 
+            txDimensionar.Location = new Point(264, 150);
+            txDimensionar.Name = "txDimensionar";
+            txDimensionar.Size = new Size(125, 27);
+            txDimensionar.TabIndex = 5;
+            // 
+            // btsalir
+            // 
+            btsalir.Location = new Point(740, 148);
+            btsalir.Name = "btsalir";
+            btsalir.Size = new Size(100, 29);
+            btsalir.TabIndex = 6;
+            btsalir.Text = "salir";
+            btsalir.UseVisualStyleBackColor = true;
+            // 
+            // btmostrar
+            // 
+            btmostrar.Location = new Point(91, 220);
+            btmostrar.Name = "btmostrar";
+            btmostrar.Size = new Size(100, 29);
+            btmostrar.TabIndex = 7;
+            btmostrar.Text = "Mostrar";
+            btmostrar.UseVisualStyleBackColor = true;
+            btmostrar.Click += btmostrar_Click;
+            // 
+            // pruebaBorrar
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.ForestGreen;
+            ClientSize = new Size(888, 460);
+            Controls.Add(btmostrar);
+            Controls.Add(btsalir);
+            Controls.Add(txDimensionar);
+            Controls.Add(btdimensionar);
+            Controls.Add(label2);
+            Controls.Add(lbresultado);
+            Controls.Add(txcargar);
+            Controls.Add(btcargar);
+            MaximizeBox = false;
+            Name = "pruebaBorrar";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "pruebaBorrar";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btcargar;
+        private TextBox txcargar;
+        private Label lbresultado;
+        private Label label2;
+        private Button btdimensionar;
+        private TextBox txDimensionar;
+        private Button btsalir;
+        private Button btmostrar;
+    }
+}
---
+++ borrarRepaso/borrarRepaso/pruebaBorrar.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace borrarRepaso
+{
+    public partial class pruebaBorrar : Form
+    {
+
+        string[] nombres;
+        int posicion;
+
+        public pruebaBorrar()
+        {
+            InitializeComponent();
+        }
+
+        private void btdimensionar_Click(object sender, EventArgs e)
+        {
+            int cantidad = Convert.ToInt32(txDimensionar.Text);
+            nombres = new string[cantidad];
+        }
+
+        private void btcargar_Click(object sender, EventArgs e)
+        {
+            nombres[posicion] = txcargar.Text;
+            posicion = posicion + 1;
+            txcargar.Focus();
+            txcargar.SelectAll();
+        }
+
+        private void btmostrar_Click(object sender, EventArgs e)
+        {
+            foreach (string contador in nombres)
+            {
+                lbresultado.Text = lbresultado.Text + contador + "\r\n" + "\r\n";
+            }
+        }
+    }
+}
