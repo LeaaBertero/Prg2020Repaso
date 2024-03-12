9d325a4e5c70dcf1bb088aa5f274f02259d0dea0
Leandro Bertero <leandro.bertero@hotmail.com>
2024-03-12 02:55:04 -0300

    metodo suma -primera parte

--- DimensionPersona/DimensionPersona/Calcu.Designer.cs
+++ DimensionPersona/DimensionPersona/Calcu.Designer.cs
@@ -29,6 +29,11 @@
         private void InitializeComponent()
         {
             btsalir = new Button();
+            btsuma = new Button();
+            txoper1 = new TextBox();
+            txoper2 = new TextBox();
+            label1 = new Label();
+            lbresultado = new Label();
             SuspendLayout();
             // 
             // btsalir
@@ -41,20 +46,73 @@
             btsalir.UseVisualStyleBackColor = true;
             btsalir.Click += btsalir_Click;
             // 
+            // btsuma
+            // 
+            btsuma.Location = new Point(17, 117);
+            btsuma.Name = "btsuma";
+            btsuma.Size = new Size(134, 34);
+            btsuma.TabIndex = 1;
+            btsuma.Text = "Calcular";
+            btsuma.UseVisualStyleBackColor = true;
+            btsuma.Click += btsuma_Click;
+            // 
+            // txoper1
+            // 
+            txoper1.Location = new Point(184, 121);
+            txoper1.Name = "txoper1";
+            txoper1.Size = new Size(125, 27);
+            txoper1.TabIndex = 2;
+            // 
+            // txoper2
+            // 
+            txoper2.Location = new Point(391, 124);
+            txoper2.Name = "txoper2";
+            txoper2.Size = new Size(125, 27);
+            txoper2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(340, 131);
+            label1.Name = "label1";
+            label1.Size = new Size(19, 20);
+            label1.TabIndex = 4;
+            label1.Text = "+";
+            // 
+            // lbresultado
+            // 
+            lbresultado.AutoSize = true;
+            lbresultado.Location = new Point(574, 131);
+            lbresultado.Name = "lbresultado";
+            lbresultado.Size = new Size(50, 20);
+            lbresultado.TabIndex = 5;
+            lbresultado.Text = "label2";
+            // 
             // Calcu
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(lbresultado);
+            Controls.Add(label1);
+            Controls.Add(txoper2);
+            Controls.Add(txoper1);
+            Controls.Add(btsuma);
             Controls.Add(btsalir);
             Name = "Calcu";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Calcu";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button btsalir;
+        private Button btsuma;
+        private TextBox txoper1;
+        private TextBox txoper2;
+        private Label label1;
+        private Label lbresultado;
     }
 }
--- DimensionPersona/DimensionPersona/Calcu.cs
+++ DimensionPersona/DimensionPersona/Calcu.cs
@@ -21,5 +21,36 @@ namespace DimensionPersona
         {
             Close();
         }
+
+        private void btsuma_Click(object sender, EventArgs e)
+        {
+            //int numero1;
+            //int numero2;
+            //int resultado;
+
+
+            //numero1 = Convert.ToInt32(txoper1.Text);
+            //numero2 = Convert.ToInt32(txoper2.Text);
+
+            //resultado = numero1 + numero2;
+
+            lbresultado.Text = Convert.ToString(Suma());
+            
+        }
+
+        public int Suma()
+        {
+            int numero1;
+            int numero2;
+            int resultado;
+
+
+            numero1 = Convert.ToInt32(txoper1.Text);
+            numero2 = Convert.ToInt32(txoper2.Text);
+
+            resultado = numero1 + numero2;
+
+            return resultado;
+        }
     }
 }
