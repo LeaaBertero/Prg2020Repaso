e494ce4024e38496f0ce1064c55f1436ff27282e
Leandro Bertero <leandro.bertero@hotmail.com>
2024-03-20 15:32:26 -0300

    Calculadora con decimales.. Finalizada

---
+++ CalcuPrueba/CalcuPrueba/CalcuPrueba.Designer.cs
@@ -0,0 +1,463 @@
+﻿namespace CalcuPrueba
+{
+    partial class CalcuPrueba
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btSuma = new Button();
+            btResta = new Button();
+            btMult = new Button();
+            btDiv = new Button();
+            txSum2 = new TextBox();
+            TxRes2 = new TextBox();
+            TxMult2 = new TextBox();
+            TxDiv2 = new TextBox();
+            txSum1 = new TextBox();
+            TxRes1 = new TextBox();
+            TxMult1 = new TextBox();
+            TxDiv1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            lbresSuma = new Label();
+            lbresResta = new Label();
+            lbresMult = new Label();
+            lbresDiv = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            btsalir = new Button();
+            SuspendLayout();
+            // 
+            // btSuma
+            // 
+            btSuma.Cursor = Cursors.Hand;
+            btSuma.Location = new Point(632, 162);
+            btSuma.Name = "btSuma";
+            btSuma.Size = new Size(118, 33);
+            btSuma.TabIndex = 3;
+            btSuma.Text = "Suma";
+            btSuma.UseVisualStyleBackColor = true;
+            btSuma.Click += btSuma_Click;
+            // 
+            // btResta
+            // 
+            btResta.Cursor = Cursors.Hand;
+            btResta.Location = new Point(632, 232);
+            btResta.Name = "btResta";
+            btResta.Size = new Size(118, 33);
+            btResta.TabIndex = 6;
+            btResta.Text = "Resta";
+            btResta.UseVisualStyleBackColor = true;
+            btResta.Click += btResta_Click;
+            // 
+            // btMult
+            // 
+            btMult.Cursor = Cursors.Hand;
+            btMult.Location = new Point(632, 307);
+            btMult.Name = "btMult";
+            btMult.Size = new Size(150, 33);
+            btMult.TabIndex = 9;
+            btMult.Text = "Multiplicación";
+            btMult.UseVisualStyleBackColor = true;
+            btMult.Click += btMult_Click;
+            // 
+            // btDiv
+            // 
+            btDiv.Cursor = Cursors.Hand;
+            btDiv.Location = new Point(632, 397);
+            btDiv.Name = "btDiv";
+            btDiv.Size = new Size(118, 33);
+            btDiv.TabIndex = 12;
+            btDiv.Text = "División";
+            btDiv.UseVisualStyleBackColor = true;
+            btDiv.Click += btDiv_Click;
+            // 
+            // txSum2
+            // 
+            txSum2.BackColor = Color.Black;
+            txSum2.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            txSum2.ForeColor = SystemColors.Control;
+            txSum2.Location = new Point(424, 162);
+            txSum2.Name = "txSum2";
+            txSum2.Size = new Size(155, 30);
+            txSum2.TabIndex = 2;
+            // 
+            // TxRes2
+            // 
+            TxRes2.BackColor = Color.Black;
+            TxRes2.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            TxRes2.ForeColor = SystemColors.Control;
+            TxRes2.Location = new Point(424, 232);
+            TxRes2.Name = "TxRes2";
+            TxRes2.Size = new Size(155, 30);
+            TxRes2.TabIndex = 5;
+            // 
+            // TxMult2
+            // 
+            TxMult2.BackColor = Color.Black;
+            TxMult2.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            TxMult2.ForeColor = SystemColors.Control;
+            TxMult2.Location = new Point(424, 308);
+            TxMult2.Name = "TxMult2";
+            TxMult2.Size = new Size(155, 30);
+            TxMult2.TabIndex = 8;
+            // 
+            // TxDiv2
+            // 
+            TxDiv2.BackColor = Color.Black;
+            TxDiv2.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            TxDiv2.ForeColor = SystemColors.Control;
+            TxDiv2.Location = new Point(424, 397);
+            TxDiv2.Name = "TxDiv2";
+            TxDiv2.Size = new Size(155, 30);
+            TxDiv2.TabIndex = 11;
+            // 
+            // txSum1
+            // 
+            txSum1.BackColor = Color.Black;
+            txSum1.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            txSum1.ForeColor = SystemColors.Control;
+            txSum1.Location = new Point(180, 162);
+            txSum1.Name = "txSum1";
+            txSum1.Size = new Size(155, 30);
+            txSum1.TabIndex = 1;
+            // 
+            // TxRes1
+            // 
+            TxRes1.BackColor = Color.Black;
+            TxRes1.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            TxRes1.ForeColor = SystemColors.Control;
+            TxRes1.Location = new Point(180, 235);
+            TxRes1.Name = "TxRes1";
+            TxRes1.Size = new Size(155, 30);
+            TxRes1.TabIndex = 4;
+            // 
+            // TxMult1
+            // 
+            TxMult1.BackColor = Color.Black;
+            TxMult1.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            TxMult1.ForeColor = SystemColors.Control;
+            TxMult1.Location = new Point(180, 310);
+            TxMult1.Name = "TxMult1";
+            TxMult1.Size = new Size(155, 30);
+            TxMult1.TabIndex = 7;
+            // 
+            // TxDiv1
+            // 
+            TxDiv1.BackColor = Color.Black;
+            TxDiv1.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            TxDiv1.ForeColor = SystemColors.Control;
+            TxDiv1.Location = new Point(180, 399);
+            TxDiv1.Name = "TxDiv1";
+            TxDiv1.Size = new Size(155, 30);
+            TxDiv1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = SystemColors.Control;
+            label1.Location = new Point(180, 136);
+            label1.Name = "label1";
+            label1.Size = new Size(90, 23);
+            label1.TabIndex = 12;
+            label1.Text = "Número 1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = SystemColors.Control;
+            label2.Location = new Point(424, 133);
+            label2.Name = "label2";
+            label2.Size = new Size(90, 23);
+            label2.TabIndex = 13;
+            label2.Text = "Número 2";
+            // 
+            // lbresSuma
+            // 
+            lbresSuma.AutoSize = true;
+            lbresSuma.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            lbresSuma.ForeColor = SystemColors.Control;
+            lbresSuma.Location = new Point(810, 166);
+            lbresSuma.Name = "lbresSuma";
+            lbresSuma.Size = new Size(59, 23);
+            lbresSuma.TabIndex = 20;
+            lbresSuma.Text = "label9";
+            lbresSuma.Visible = false;
+            // 
+            // lbresResta
+            // 
+            lbresResta.AutoSize = true;
+            lbresResta.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            lbresResta.ForeColor = SystemColors.Control;
+            lbresResta.Location = new Point(810, 236);
+            lbresResta.Name = "lbresResta";
+            lbresResta.Size = new Size(69, 23);
+            lbresResta.TabIndex = 21;
+            lbresResta.Text = "label10";
+            lbresResta.Visible = false;
+            // 
+            // lbresMult
+            // 
+            lbresMult.AutoSize = true;
+            lbresMult.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            lbresMult.ForeColor = SystemColors.Control;
+            lbresMult.Location = new Point(810, 313);
+            lbresMult.Name = "lbresMult";
+            lbresMult.Size = new Size(69, 23);
+            lbresMult.TabIndex = 22;
+            lbresMult.Text = "label11";
+            lbresMult.Visible = false;
+            // 
+            // lbresDiv
+            // 
+            lbresDiv.AutoSize = true;
+            lbresDiv.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            lbresDiv.ForeColor = SystemColors.Control;
+            lbresDiv.Location = new Point(810, 405);
+            lbresDiv.Name = "lbresDiv";
+            lbresDiv.Size = new Size(69, 23);
+            lbresDiv.TabIndex = 23;
+            lbresDiv.Text = "label12";
+            lbresDiv.Visible = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = SystemColors.Control;
+            label3.Location = new Point(424, 206);
+            label3.Name = "label3";
+            label3.Size = new Size(90, 23);
+            label3.TabIndex = 25;
+            label3.Text = "Número 2";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = SystemColors.Control;
+            label4.Location = new Point(180, 208);
+            label4.Name = "label4";
+            label4.Size = new Size(90, 23);
+            label4.TabIndex = 24;
+            label4.Text = "Número 1";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.ForeColor = SystemColors.Control;
+            label5.Location = new Point(424, 282);
+            label5.Name = "label5";
+            label5.Size = new Size(90, 23);
+            label5.TabIndex = 27;
+            label5.Text = "Número 2";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.ForeColor = SystemColors.Control;
+            label6.Location = new Point(180, 284);
+            label6.Name = "label6";
+            label6.Size = new Size(90, 23);
+            label6.TabIndex = 26;
+            label6.Text = "Número 1";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.ForeColor = SystemColors.Control;
+            label7.Location = new Point(424, 370);
+            label7.Name = "label7";
+            label7.Size = new Size(90, 23);
+            label7.TabIndex = 29;
+            label7.Text = "Número 2";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.ForeColor = SystemColors.Control;
+            label8.Location = new Point(180, 373);
+            label8.Name = "label8";
+            label8.Size = new Size(90, 23);
+            label8.TabIndex = 28;
+            label8.Text = "Número 1";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = Color.Black;
+            label9.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label9.ForeColor = Color.Snow;
+            label9.Location = new Point(370, 167);
+            label9.Name = "label9";
+            label9.Size = new Size(26, 28);
+            label9.TabIndex = 30;
+            label9.Text = "+";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = Color.Black;
+            label10.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label10.ForeColor = Color.Snow;
+            label10.Location = new Point(370, 238);
+            label10.Name = "label10";
+            label10.Size = new Size(20, 28);
+            label10.TabIndex = 31;
+            label10.Text = "-";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.BackColor = Color.Black;
+            label11.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label11.ForeColor = Color.Snow;
+            label11.Location = new Point(370, 314);
+            label11.Name = "label11";
+            label11.Size = new Size(21, 28);
+            label11.TabIndex = 32;
+            label11.Text = "*";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.BackColor = Color.Black;
+            label12.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label12.ForeColor = Color.Snow;
+            label12.Location = new Point(370, 397);
+            label12.Name = "label12";
+            label12.Size = new Size(21, 28);
+            label12.TabIndex = 33;
+            label12.Text = "/";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.BackColor = Color.DarkOrange;
+            label13.Font = new Font("Segoe UI", 18F);
+            label13.ForeColor = SystemColors.Control;
+            label13.Location = new Point(370, 33);
+            label13.Name = "label13";
+            label13.Size = new Size(370, 41);
+            label13.TabIndex = 34;
+            label13.Text = "Calculadora con decimales";
+            // 
+            // btsalir
+            // 
+            btsalir.Cursor = Cursors.Hand;
+            btsalir.Location = new Point(442, 470);
+            btsalir.Name = "btsalir";
+            btsalir.Size = new Size(118, 33);
+            btsalir.TabIndex = 13;
+            btsalir.Text = "Salir";
+            btsalir.UseVisualStyleBackColor = true;
+            btsalir.Click += btsalir_Click;
+            // 
+            // CalcuPrueba
+            // 
+            AutoScaleDimensions = new SizeF(10F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Goldenrod;
+            ClientSize = new Size(1148, 518);
+            Controls.Add(btsalir);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label7);
+            Controls.Add(label8);
+            Controls.Add(label5);
+            Controls.Add(label6);
+            Controls.Add(label3);
+            Controls.Add(label4);
+            Controls.Add(lbresDiv);
+            Controls.Add(lbresMult);
+            Controls.Add(lbresResta);
+            Controls.Add(lbresSuma);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(TxDiv1);
+            Controls.Add(TxMult1);
+            Controls.Add(TxRes1);
+            Controls.Add(txSum1);
+            Controls.Add(TxDiv2);
+            Controls.Add(TxMult2);
+            Controls.Add(TxRes2);
+            Controls.Add(txSum2);
+            Controls.Add(btDiv);
+            Controls.Add(btMult);
+            Controls.Add(btResta);
+            Controls.Add(btSuma);
+            Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            Name = "CalcuPrueba";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "CalcuPrueba";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btSuma;
+        private Button btResta;
+        private Button btMult;
+        private Button btDiv;
+        private TextBox txSum2;
+        private TextBox TxRes2;
+        private TextBox TxMult2;
+        private TextBox TxDiv2;
+        private TextBox txSum1;
+        private TextBox TxRes1;
+        private TextBox TxMult1;
+        private TextBox TxDiv1;
+        private Label label1;
+        private Label label2;
+        private Label lbresSuma;
+        private Label lbresResta;
+        private Label lbresMult;
+        private Label lbresDiv;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Button btsalir;
+    }
+}
---
+++ CalcuPrueba/CalcuPrueba/CalcuPrueba.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcuPrueba
+{
+    public partial class CalcuPrueba : Form
+    {
+        public CalcuPrueba()
+        {
+            InitializeComponent();
+        }
+
+        private void btSuma_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal numSuma1;
+                decimal numSuma2;
+
+                decimal resSuma;
+
+                numSuma1 = Convert.ToDecimal(txSum1.Text);
+                numSuma2 = Convert.ToDecimal(txSum2.Text);
+
+                resSuma = numSuma1 + numSuma2;
+
+                lbresSuma.Text = "El resultado es: " + " " + resSuma.ToString();
+
+                lbresSuma.Visible = true;
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("No puede dejar los campos vacios");
+            }
+
+        }
+
+        private void btResta_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal numRes1;
+                decimal numRes2;
+
+                decimal resResta;
+
+                numRes1 = Convert.ToDecimal(TxRes1.Text);
+                numRes2 = Convert.ToDecimal(TxRes2.Text);
+
+                resResta = numRes1 - numRes2;
+
+                lbresResta.Text = "El resultado es: " + " " + resResta.ToString();
+
+                lbresResta.Visible = true;
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("No puede dejar los campos vacios");
+            }
+        }
+
+        private void btMult_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal numMult1;
+                decimal numMult2;
+
+                decimal resMult;
+
+                numMult1 = Convert.ToDecimal(TxMult1.Text);
+                numMult2 = Convert.ToDecimal(TxMult2.Text);
+
+                resMult = numMult1 * numMult2;
+
+                lbresMult.Text = "El resultado es: " + " " + resMult.ToString();
+
+                lbresMult.Visible = true;
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("No puede dejar los campos vacios");
+            }
+        }
+
+        private void btDiv_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal numDiv1;
+                decimal numDiv2;
+
+                decimal resDiv;
+
+                numDiv1 = Convert.ToDecimal(TxDiv1.Text);
+                numDiv2 = Convert.ToDecimal(TxDiv2.Text);
+
+                resDiv = numDiv1 / numDiv2;
+
+                lbresDiv.Text = "El resultado es: " + " " + resDiv.ToString();
+
+                lbresDiv.Visible = true;
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("No puede dejar los campos vacios");
+            }
+        }
+
+        private void btsalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
